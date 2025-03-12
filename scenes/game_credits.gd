c026dcbd02dae6b7d1cbbf43f39e1a78e3ea89ee
Robin64-wag <robinwagner07@gmail.com>
2025-03-12 17:21:38 +0100

    Credits UI

---
+++ scenes/game_credits.gd
@@ -0,0 +1,13 @@
+extends VBoxContainer
+
+## maps the index of a locale to the locale itself
+var locales:PackedStringArray = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
