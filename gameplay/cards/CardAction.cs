2b931103c21bb8e41dcdeacd0391e7cd9c40dc0e
Florian Oetke <lowkey@second-system.de>
2025-03-13 18:48:44 +0100

    feat: looting

--- gameplay/cards/CardAction.cs
+++ gameplay/cards/CardAction.cs
@@ -6,19 +6,8 @@ namespace CrimeChaosAndCatnip;
 public partial class CardAction : CardBase {
 
 	public override BoardObject.InteractResult PlayedCardInteraction(PlayedCard card, Unit unit) {
-		if (unit is not PlayerUnit p || card.Board==null)
+		if (unit is not PlayerUnit p || !p.CanAttack || card.Board==null)
 			return BoardObject.InteractResult.Ignored;
-
-		// if the unit is on a LootSource => farm loot
-		var cell = card.Board.TryGetCell(card.BoardPosition);
-		if (cell != null) {
-			for (var i = cell.Objects.Count - 1; i >= 0; i--) {
-				var obj = cell.Objects[i];
-				if (obj is LootSource ls) {
-					ls.TryLoot(p);
-				}
-			}
-		}
 		
 		// look at the 3x3 fields around the unit and destroy all enemies and destructure objects
 		for (var x = -1; x <= 1; x++) {
@@ -26,9 +15,12 @@ public partial class CardAction : CardBase {
 				var boardPosition = card.BoardPosition;
 				boardPosition.X += x;
 				boardPosition.Y += y;
-				cell = card.Board.TryGetCell(boardPosition);
+				var cell = card.Board.TryGetCell(boardPosition);
 				if (cell != null) {
 					foreach (var obj in cell.Objects) {
+						if (obj is LootSource ls) {
+							ls.TryLoot(p);
+						}
 						if (obj is EnemyUnit enemy) {
 							enemy.Kill();
 						}
