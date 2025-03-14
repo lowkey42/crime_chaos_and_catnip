b0779117eb7fe4367c98ab0328038f62d386ed35
Florian Oetke <lowkey@second-system.de>
2025-03-14 21:41:33 +0100

    feat: tweaked card shader

--- ui/card_shader_helper.gd
+++ ui/card_shader_helper.gd
@@ -2,11 +2,18 @@ extends TextureRect
 
 var is_mouse_inside = false
 
-var mouse_pos : Vector2
+var mouse_pos := Vector2.ZERO
+var intensity := 0
 
 func _process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	var relative_mouse_position = mouse_position - global_position
 	var s = scale * get_parent().scale
-	mouse_pos = lerp(mouse_pos, relative_mouse_position/s - size/2.0 if get_global_rect().has_point(mouse_position) else Vector2(0.0,0.0), 0.2)
-	material.set_shader_parameter("_mousePos", relative_mouse_position/s - size/2.0)
+	if get_global_rect().expand(get_global_rect().size*2.0).has_point(mouse_position):
+		mouse_pos = relative_mouse_position/s - size/2.0
+		intensity = 1.0
+	else:
+		intensity = max(0.0, intensity - delta/4.0)
+
+	material.set_shader_parameter("_mousePos", mouse_pos)
+	material.set_shader_parameter("_intensity", intensity)
