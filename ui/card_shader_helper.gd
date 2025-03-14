0d89cca6833b9e5674fc5b0df91e365e5a6b9d56
Florian Oetke <lowkey@second-system.de>
2025-03-14 21:55:51 +0100

    fix: limit card shine to hovered cards

--- ui/card_shader_helper.gd
+++ ui/card_shader_helper.gd
@@ -9,11 +9,11 @@ func _process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	var relative_mouse_position = mouse_position - global_position
 	var s = scale * get_parent().scale
-	if get_global_rect().expand(get_global_rect().size*2.0).has_point(mouse_position):
+	if get_global_rect().has_point(mouse_position):
 		mouse_pos = relative_mouse_position/s - size/2.0
 		intensity = 1.0
 	else:
-		intensity = max(0.0, intensity - delta/4.0)
+		intensity = max(0.0, intensity - delta/2.0)
 
 	material.set_shader_parameter("_mousePos", mouse_pos)
 	material.set_shader_parameter("_intensity", intensity)
